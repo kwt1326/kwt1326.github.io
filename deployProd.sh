7a91b57aafa261f05c2105b3d632b1e7a5e43ca3
kwt1326 <u1326@hotmail.com>
2021-04-13 17:17:58 +0900

    test

--- deployProd.sh
+++ deployProd.sh
@@ -1,11 +1,11 @@
 #!bin/sh
 
 rm -rf node_modules/.cache
-next build
+npm run build
 next export
 touch out/.nojekyll
 git add out/
-git commit -m \"Deploy Next.js to gh-pages\"
-git subtree push --prefix out origin gh-pages
+git commit -m \"Deploy Next.js to master\"
+git subtree push --prefix out origin master
 
 # next.js official deploy example 
