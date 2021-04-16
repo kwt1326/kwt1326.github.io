bca7aecbdc26ac56007e6ff5ad131a70d6201f49
kwt1326 <u1326@hotmail.com>
2021-04-16 17:35:23 +0900

    test

--- deployProd.sh
+++ deployProd.sh
@@ -7,6 +7,6 @@ mkdir out
 touch out/.nojekyll
 git add out/
 git commit -m 'Deploy Next.js to gh-pages'
-git subtree push --prefix out origin gh-pages
+git subtree push --prefix out gh-pages
 
 # next.js official deploy example 
