03500f3dee9d88a86767519958639583c9f41dbc
kwt1326 <u1326@hotmail.com>
2021-04-16 17:40:14 +0900

    test

--- next.config.js
+++ next.config.js
@@ -1,9 +1,6 @@
 const webpack = require('webpack');
 
 module.exports = {
-  future: {
-    webpack5: true,
-  },
   // webpack: (config, _options) => {
   //   config.plugins.push(new webpack.IgnorePlugin(/editor/));
   //   return config;
