4c4cd42e4eb36deb24547c0ecd27506ac90b2c6b
kwt1326 <u1326@hotmail.com>
2021-04-20 17:54:19 +0900

    test

--- components/Header/index.tsx
+++ components/Header/index.tsx
@@ -4,7 +4,7 @@ import styles from './Header.module.scss';
 const Header = (props: any) => {
   return (
     <header className={styles.container}>
-      <div className={styles.title}>Kim 동태 's blog</div>
+      <div className={styles.title}>동태 's blog</div>
       <div className={styles.right_menu}>
         <img src={'/images/menu.svg'} />
       </div>
