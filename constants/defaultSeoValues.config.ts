c2141860ffd414f3ef03b9719b512bd2f76f46c7
kwt1326 <u1326@hotmail.com>
2021-11-01 13:24:32 +0900

    remove _document.tsx

--- constants/defaultSeoValues.config.ts
+++ constants/defaultSeoValues.config.ts
@@ -1,17 +1,15 @@
 const DefaultSeoValues = {
   titleTemplate: "%s | wontae_code Dev blog",
+  canonical: "https://kwt1326.github.io",
   openGraph: {
+    images: [{ url: "/images/og_logo.png" }],
     type: 'website',
     locale: 'ko_KR',
     url: 'https://kwt1326.github.io/',
     title: "wontae_code Dev blog",
     site_name: "wontae_code Dev blog",
-    description: 'wontae Kim 의 개발 블로그에 오신걸 환영해요.'
+    description: 'wontae Kim 의 개발 블로그에 오신걸 환영해요.'    
   },
-  // twitter: {
-  //   handle: '@garmeeh',
-  //   cardType: 'summary_large_image',
-  // },
 };
 
 export default DefaultSeoValues;
