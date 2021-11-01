cd03e9b8f24e588f22178f028dbe9e27b2f5088b
kwt1326 <u1326@hotmail.com>
2021-11-01 09:48:24 +0900

    posting

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -18,16 +18,7 @@ export default class MyDocument extends Document {
           <meta
             name="og:description"
             content="wontae Kim 의 개발 블로그에 오신걸 환영해요."
-          />
-          {/* <meta
-            name="og:image"
-            content="/images/og_image.png"
-          /> */}
-
-          {/* <link rel="icon" type="image/png" sizes="16x16" href={"/images/favicon-16x16.png"}/>
-          <link rel="icon" type="image/png" sizes="32x32" href={"/images/favicon-32x32.png"}/>
-          <link rel="icon" type="image/png" sizes="96x96" href={"/images/favicon-96x96.png"}/> */}
-          
+          />         
           <link rel="canonical" href="https://kwt1326.github.io" />
         </Head>
         <body>
