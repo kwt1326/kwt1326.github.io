c99b706b55aad12e8a6a47e553ebc9ae0e7f3a53
kwt1326 <u1326@hotmail.com>
2021-11-29 13:59:28 +0900

    fixed

--- pages/about_me/index.tsx
+++ pages/about_me/index.tsx
@@ -1,5 +1,6 @@
 import { readFileSync } from 'fs';
 import { GetStaticProps } from 'next';
+import { NextSeo } from 'next-seo';
 import { withRouter } from 'next/router';
 import path from 'path';
 import React from 'react';
@@ -10,6 +11,11 @@ import styles from './Aboutme.module.scss';
 const AboutMe = (props: any) => {
   return (
     <section className={styles.container}>
+      <NextSeo
+        title={'wontae Kim\'s about me'}
+        description={'+3년차 웹 개발자 wontae Kim 의 자기소개 페이지 입니다!'}
+        canonical={'https://kwt1326.github.io'}
+      />
       <Viewer initialValue={props?.content || ''} noContainerStyle={true} />
     </section>
   )
