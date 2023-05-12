6d8506724ff7f974c80fbfd08f8f6aaa982e40e6
kwt1326 <u1326@hotmail.com>
2023-05-12 21:55:42 +0900

    migration: turborepo, next.js, strapi

---
+++ apps/web/src/app/blog/post/[slug]/page.tsx
@@ -0,0 +1,29 @@
+import { getPost, getPosts } from "@/data/source/post";
+
+// export async function generateStaticParams() {
+//   return await getPosts();
+// }
+
+export async function generateMetadata({ params }: any) {
+  const post: any = await getPost(params.slug);
+  return { title: 'Blog', description: post.title };
+}
+
+export default async function Post({ params }: any) {
+  const post: any = await getPost(params.slug);
+
+  if (!post) {
+    return {
+      status: 404,
+    };
+  }
+
+  const components = {};
+
+  return (
+    <main>
+      <div>{post.author.name}</div>
+      <h1>{post.title}</h1>
+    </main>
+  );
+}
