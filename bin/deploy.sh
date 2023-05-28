15649e7a86afdb264be0d8d8c13dc0695661999c
kwt1326 <u1326@hotmail.com>
2023-05-28 16:54:18 +0900

    fix: deploy

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -12,9 +12,12 @@ git commit -m 'Deploy Next.js to gh-pages'
 
 cd ../../
 
-git add -f apps/blog-cms/public/uploads/
-git subtree push --prefix apps/blog-cms/public/uploads/ origin resources
 git subtree push --prefix apps/web/out origin gh-pages
 git push
+
+# resource update
+git add -f apps/blog-cms/public/uploads/
+git subtree push --prefix apps/blog-cms/public/uploads/ origin resources
+git reset head
 
 # deploy branch : gh-pages
